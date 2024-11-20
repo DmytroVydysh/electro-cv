7c4653d07eec89aa79963dde3c26496354c824b0
Dmytro <dmytrovydysh271098@gmail.com>
2024-11-20 23:55:47 +0100

    new header

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,5 +1,5 @@
 import { MoonStars, SignIn, Sun, UserCircleCheck, UserCirclePlus } from "@phosphor-icons/react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from 'react-i18next';
 
 
@@ -17,7 +17,12 @@ interface PublicLayoutProps {
 export const PublicLayout: React.FC<PublicLayoutProps> = ({ children }) => {
   const { t, i18n: { language, changeLanguage } } = useTranslation();
   const dispatch = useDispatch();
+  const [sidebarDisplay, setSidebarDisplay] = useState<'none' | 'block'>('none');
   const currentPageName = useSelector((state: RootState) => state.page.name);
+
+  const handleToggleSidebar = () => {
+    setSidebarDisplay(sidebarDisplay === 'none' ? 'block' : 'none');
+  };
 
   useEffect(() => {
     const theme = localStorage.getItem('theme');
@@ -44,7 +49,7 @@ export const PublicLayout: React.FC<PublicLayoutProps> = ({ children }) => {
 
   return (
     <>
-      <header className="sticky top-0 w-full bg-primary shadow-md z-50" style={{ height: '3.5rem' }}>
+      <header className="sticky top-0 w-full bg-primary shadow-md z-50 hidden" style={{ height: '3.5rem' }}>
         <div className="px-2 flex items-center justify-between w-full h-full">
 
           {/* Logo o Titolo - cambia a seconda delle dimensioni */}
@@ -98,8 +103,8 @@ export const PublicLayout: React.FC<PublicLayoutProps> = ({ children }) => {
 
             <div className="inline-flex items-center">
 
-              <div className="dropdown h-full inline-flex items-center">
-                <button tabIndex={0} role="button" className="btn lg:btn-md btn-sm rounded-r-none"><img src={`/images/flags/${language}.svg`} alt="" /></button>
+              <div className="dropdown">
+                <button tabIndex={0} role="button" className="btn lg:btn-md btn-sm rounded-r-none"><img src={`/images/flags/${language}.svg`} alt="" style={{ maxWidth: '24px', maxHeight: '24px' }} /></button>
                 <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow absolute right-0">
                   <li><a href="/" onClick={(event: React.MouseEvent<HTMLAnchorElement> | React.TouchEvent<HTMLAnchorElement>) => { event.preventDefault(); _changeLanguage('en'); }}><img src={`/images/flags/en.svg`} alt="" />English</a></li>
                   <li><a href="/" onClick={(event: React.MouseEvent<HTMLAnchorElement> | React.TouchEvent<HTMLAnchorElement>) => { event.preventDefault(); _changeLanguage('it'); }}><img src={`/images/flags/it.svg`} alt="" />Italiano</a></li>
@@ -107,7 +112,7 @@ export const PublicLayout: React.FC<PublicLayoutProps> = ({ children }) => {
                 </ul>
               </div>
 
-              <div className="dropdown h-full inline-flex items-centers">
+              <div className="dropdown">
                 <button tabIndex={0} role="button" className="btn lg:btn-md btn-sm rounded-l-none"><SignIn size={24} /></button>
                 <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow absolute right-0">
                   <li><a href="/sign-in"><UserCircleCheck size={24} />{t('header_signin')}</a></li>
@@ -119,6 +124,72 @@ export const PublicLayout: React.FC<PublicLayoutProps> = ({ children }) => {
           </div>
         </div>
       </header>
+
+      <header className='flex border-b py-4 px-4 sm:px-10 bg-primary font-[sans-serif] min-h-[70px] tracking-wide relative z-50' style={{height:'80px', maxHeight: '80px'}}>
+        <div className='flex flex-wrap items-center gap-5 w-full'>
+          <a href="/"><img src="https://readymadeui.com/readymadeui.svg" alt="logo" className='w-36' /> </a>
+
+          <div id="collapseMenu" className='max-lg:hidden lg:!block max-lg:w-full max-lg:fixed max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50' style={{ display: sidebarDisplay }}>
+            <button id="toggleClose" className='lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white p-3'  onClick={handleToggleSidebar}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-4 fill-black" viewBox="0 0 320.591 320.591">
+                <path d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z" data-original="#000000"></path>
+                <path d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z" data-original="#000000"></path>
+              </svg>
+            </button>
+
+            <ul className='lg:flex lg:ml-14 lg:gap-x-5 max-lg:space-y-3 max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50'>
+              <li className='mb-6 hidden max-lg:block'>
+                <a href="/">
+                  <img src="https://readymadeui.com/readymadeui.svg" alt="logo" className='w-36' />
+                </a>
+              </li>
+              <li className='max-lg:border-b max-lg:py-3 px-3'>
+                <a href='/' className='lg:hover:text-[#007bff] text-[#007bff] block font-semibold text-[15px]'>Home</a>
+              </li>
+              <li className='max-lg:border-b max-lg:py-3 px-3'>
+                <a href='/' className='lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]'>Team</a>
+              </li>
+              <li className='max-lg:border-b max-lg:py-3 px-3'>
+                <a href='/' className='lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]'>Feature</a>
+              </li>
+              <li className='max-lg:border-b max-lg:py-3 px-3'>
+                <a href='/' className='lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]'>Blog</a>
+              </li>
+              <li className='max-lg:border-b max-lg:py-3 px-3'>
+                <a href='/' className='lg:hover:text-[#007bff] text-gray-500 block font-semibold text-[15px]'>About</a>
+              </li>
+            </ul>
+          </div>
+          <div className="dropdown">
+            <button tabIndex={0} role="button" className="btn lg:btn-md btn-sm bg-transparent border-0"><img src={`/images/flags/${language}.svg`} alt="" style={{ maxWidth: '24px', maxHeight: '24px' }} /></button>
+            <ul tabIndex={0} className="dropdown-content menu z-[1] w-52 p-2 shadow absolute right-0 bg-primary">
+              <li><a href="/" onClick={(event: React.MouseEvent<HTMLAnchorElement> | React.TouchEvent<HTMLAnchorElement>) => { event.preventDefault(); _changeLanguage('en'); }}><img src={`/images/flags/en.svg`} alt="" />English</a></li>
+              <li><a href="/" onClick={(event: React.MouseEvent<HTMLAnchorElement> | React.TouchEvent<HTMLAnchorElement>) => { event.preventDefault(); _changeLanguage('it'); }}><img src={`/images/flags/it.svg`} alt="" />Italiano</a></li>
+              <li><a href="/" onClick={(event: React.MouseEvent<HTMLAnchorElement> | React.TouchEvent<HTMLAnchorElement>) => { event.preventDefault(); _changeLanguage('ua'); }}><img src={`/images/flags/ua.svg`} alt="" />Українська</a></li>
+            </ul>
+          </div>
+          <label className="swap swap-rotate me-5">
+              {/* this hidden checkbox controls the state */}
+              <input type="checkbox" className="theme-controller" name="themechange" onChange={_changeTheme} />
+
+              {/* sun icon */}
+              <Sun className="swap-off h-10 w-10 fill-current" />
+
+              {/* moon icon */}
+              <MoonStars className="swap-on h-10 w-10 fill-current" />
+            </label>
+          <button id="toggleOpen" className='lg:hidden ml-auto' onClick={handleToggleSidebar}>
+            <svg className="w-7 h-7" fill="#000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"></path>
+            </svg>
+          </button>
+
+
+        </div>
+      </header>
       {children}
-    </>);
+    </>
+  );
 }
--- src/pages/public/Slash/index.tsx
+++ src/pages/public/Slash/index.tsx
@@ -19,12 +19,7 @@ export const Slash: React.FC = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="relative isolate bg-gray-900 py-24 sm:py-32 w-full" style={{ height: 'calc(100vh - 3.5rem)', maxHeight: 'calc(100vh - 3.5rem)', overflowX: 'hidden', overflowY:'auto', backgroundImage: 'url(/images/backgrounds/bg4.jpg)' }}>
-      <img
-        alt=""
-        src="/images/backgrounds/bg4.jpg"
-        className="absolute inset-0 -z-10 size-full object-cover object-right md:object-center hidden"
-      />
+    <div className="relative isolate bg-gray-900 py-24 sm:py-32 w-full slash-content-background" >
       <div aria-hidden="true" className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl" >
         <div style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)', }} className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" />
       </div>
@@ -34,26 +29,26 @@ export const Slash: React.FC = () => {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-5xl font-semibold tracking-tight text-white sm:text-7xl text-black-shadow" >{t('slash_automatize_job_research')}</h2>
-          <p className="mt-8 text-pretty text-lg font-medium text-gray-300 sm:text-xl/8 text-black-shadow" >
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-            fugiat veniam occaecat fugiat.
-          </p>
+          <p className="mt-8 text-pretty text-lg font-medium text-gray-300 sm:text-xl/8 text-black-shadow" >{t('slash_find_your_dream_job')}</p>
         </div>
         <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
-          <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base/7 font-semibold text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-            {links.map((link) => (
-              <a key={link.name} href={link.href}>
-                {link.name} <span aria-hidden="true">&rarr;</span>
-              </a>
-            ))}
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base/7 font-semibold text-white sm:grid-cols-2 md:flex lg:gap-x-10 text-black-shadow">
+            <a key={'services'} href="/services#cv" className="text-black-shadow">
+              {t('slash_services_cv')}
+              &nbsp;
+              <span aria-hidden="true">&rarr;</span>
+            </a>
+            <a key={'services3'} href="/services#cv" className="text-black-shadow">
+              {t('slash_services_cv')}
+              &nbsp;
+              <span aria-hidden="true">&rarr;</span>
+            </a>
           </div>
           <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
-            {stats.map((stat) => (
-              <div key={stat.name} className="flex flex-col-reverse gap-1 text-black-shadow">
-                <dt className="text-base/7 text-gray-300 text-black-shadow">{stat.name}</dt>
-                <dd className="text-4xl font-semibold tracking-tight text-white text-black-shadow">{stat.value}</dd>
-              </div>
-            ))}
+            <div key={'asd'} className="flex flex-col-reverse gap-1 text-black-shadow">
+              <dt className="text-base/7 text-gray-300 text-black-shadow">{'asdf'}</dt>
+              <dd className="text-4xl font-semibold tracking-tight text-white text-black-shadow">{'asdf'}</dd>
+            </div>
           </dl>
         </div>
       </div>
